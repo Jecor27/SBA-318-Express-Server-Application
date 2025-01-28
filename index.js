f7f6743d6193e2a71cde55af291a8fa2a6fb3572
Jesus <jjcorrales76@gmail.com>
2025-01-27 19:12:57 -0700

    added more

--- index.js
+++ index.js
@@ -6,23 +6,40 @@ app.set('view engine', 'ejs');
 const PORT = 8080;
 
 app.use(express.json())
-
+//middleware
 app.use((req, res, next) => {
     console.log(req.path, req.method)
     next()
 })
 
+app.use('/api/jokes', (req, res, next) => {
+    if (req.method === 'POST' && !req.body.joke) {
+        return res.status(400).json({ error: 'Joke is required' });
+    }
+    next();
+});
+
+
 app.get('/', (req, res) => {
     let data = {
         name: 'jokesss',
-        hobbies: ['playing football', 'playing chess', 'cycling']
+        jokes: []
     }
 
     res.render('home', { data: data });
 });
 
-app.use('/api/jokes', jokesRoutes)
+app.use('/api', jokesRoutes)
 
+
+app.use((req, res, next) => {
+    next(error(404, "Resource Not Found"));
+  });
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({ error: err.message });
+  });
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}`);
--- routes/jokes.js
+++ routes/jokes.js
@@ -3,6 +3,18 @@ const express = require('express')
 
 const router = express.Router()
 
+const data = {
+    jokes: [{
+        id: 1,
+        content:" Why don’t eggs tell jokes? Because they might crack up."
+    }],
+    users: [{
+        id: 1,
+        name: "Jesus mcduffle",
+        email: "example@example.com"
+    }],
+}
+
 router.get('/', (req, res) => {
     res.json({ messg: 'GET all phrases' })
 
@@ -13,8 +25,14 @@ router.get('/:id', (req, res) => {
     res.json({ messg: 'GET a single phrase' })
 })
 
-router.post('/', (req, res) => {
-    res.json({ messg: 'POST a new phrase' })
+router.post('/jokes', (req, res) => {
+    const newJoke = {
+        id: data.jokes.length + 1,
+        content: req.body.joke
+    }
+    data.jokes.push(newJoke);
+    res.status(201).json(newJoke);
+    //res.json({ messg: 'POST a new phrase' })
 })
 
 router.delete('/:id', (req, res) => {
